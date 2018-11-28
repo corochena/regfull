cb30a7713100d219a98ba23c075cf3c6a14431f9
Carlos Peña <corochena@gmail.com>
2018-11-28 17:41:54 -0600

    Agregamos las funciones de regresion multiple como comentarios

--- js/interface.js
+++ js/interface.js
@@ -80,3 +80,88 @@ function equation(coefs) {
     return str;
   },"")
 }
+
+/*
+var order=2, n=9;
+
+loadSample([[0,0,1,2,0,1,2,2,1],[0,2,2,4,4,6,6,2,1],[14,21,11,12,23,23,14,6,11]]);
+
+function crearTabla(rows, cols) {
+  var esp = document.querySelector(".datainput");
+  if (esp.firstChild) esp.removeChild(esp.firstChild);
+
+  var tabla = document.createElement("table");
+
+  for (var i = 0; i <= rows; i++) {
+    var fila = document.createElement("tr");
+    for (var j = 0; j <= cols; j++) {
+      var celda = document.createElement("td");
+      if (i==0) {
+        if (j>0)
+          celda.innerHTML = (j==cols ? "Y":"X<sub>" + j +"</sub>");
+      }
+      else
+        celda.innerHTML = (j==0 ? "<sub>" + i + "</sub>": "<input>");
+      fila.append(celda);
+    }
+    tabla.append(fila);
+  }
+
+  esp.append(tabla);
+}
+
+document.querySelector("#order").addEventListener("input", function(e) {
+  order = +e.target.value;
+  crearTabla(n,order+1);
+});
+
+document.querySelector("#n").addEventListener("input", function(e) {
+  n = +e.target.value;
+  crearTabla(n,order+1);
+});
+
+
+function loadSample(sample) {
+  var cols = sample.length;
+  var rows = sample[0].length;
+  crearTabla(rows,cols);
+  
+  var elementos = document.querySelectorAll("table input");
+  
+  for (var i=0; i<cols; i++)
+    for (var j=0; j<rows; j++)
+      elementos[i+cols*j].value = sample[i][j];
+}
+
+document.querySelector("button").addEventListener("click", function() {
+  var data = [];
+  
+  var elementos = document.querySelectorAll("table input");
+
+  for (var i = 0; i < elementos.length; i++) {
+    var v = +elementos[i].value;
+    if (i <= order) {
+      data.push([]);
+      data[i].push(v);
+    }
+    else
+      data[i%(order+1)].push(v);
+  }
+
+  var reg = regresion(data);
+  
+  var results = document.querySelector("#results");
+  var txt = "<h1>Resultados</h1>Ecuación: " + equation(reg[0]) + "<br>";
+  txt += "Error estándar: " + reg[1].toFixed(3) + "<br>";
+  txt += "R cuadrado: " + reg[2].toFixed(3);
+  results.innerHTML = txt;
+});
+
+function equation(coefs) {
+  return coefs.reduce(function(str,coef,i) {
+    str += (coef > 0 ? "+ ":"- ") + Math.abs(coef.toFixed(3)) + " ";
+    if (i>0) str += "X<sub>" + i + "</sub> ";
+    return str;
+  },"")
+}
+*/
--- js/regresion.js
+++ js/regresion.js
@@ -86,3 +86,66 @@ function gauss(A) {
   }
   return X;
 }
+
+/*
+function regresion(data) {
+  var coefs;
+  var Se = 0, St = 0, yAvg = 0;
+  var errorStd, Rcuad;
+  
+  var order = data.length - 1;
+  var n = data[0].length;
+  
+  var A = [];
+  for (var i=0; i<=order; i++) {
+    A.push([]);
+    for (var j=0; j<=order+1; j++)
+      A[i].push(0);
+  }
+       
+  var ones = [];
+  for (var i=0; i<n; i++) ones.push(1);
+  
+  var X = Array.from(data);
+  var Y = X.pop();
+  X.unshift(ones);
+  
+  for (var i=0; i<= order; i++) {
+    for (var j=0; j<=i; j++) {
+      var sum = 0;
+      for (var l=0; l<n; l++) {
+        yAvg += Y[i] / n;
+        sum += X[i][l] * X[j][l];
+      }
+      A[i][j] = sum;
+      A[j][i] = sum;
+    }
+    sum = 0;
+    for (var l=0; l<n; l++)
+      sum += Y[l]*X[i][l];
+    A[i][order+1] = sum;
+  }
+  
+  coefs = gauss(A);
+  
+  for (var i=0; i<n; i++) {
+    Se += Math.pow(Y[i]-sumprod(point(X,i),coefs),2);
+    St += Math.pow(Y[i] - yAvg, 2);
+  }
+  
+  return [coefs, Math.sqrt(Se/(n-order-1)), 1-Se/St];
+}
+
+function point(X,pos) {
+  return X.reduce(function(XatPos,currX){
+    XatPos.push(currX[pos]);
+    return XatPos;
+  },[]);
+}
+
+function sumprod(a,b){
+  return a.reduce(function(sum,x,i) {
+    return sum + x*b[i];
+  },0);
+}
+*/
